9a8530bd860e7aadf958214f26dbddd0029effde
Daniel Alexander <5389473+DanAlexand@users.noreply.github.com>
2024-04-03 18:46:34 +1300

    updating score and sighers in page currently complete

--- static/js/script.js
+++ static/js/script.js
@@ -65,11 +65,11 @@ function remove_row(removal_row_id){
 
 //Define the options for shot values. TODO: Switch case for 6 & V enabling when changing shooter class type between TR and F-Class
 let shots_input = document.getElementById('shots_input_')
-function add_shot(new_row_id, i){
+function add_shot(new_row_id, position){
     shots_input = document.getElementById('shots_input_' + new_row_id)
     console.log("create_shot row " + new_row_id)
     let select_shot = document.createElement('select')
-    select_shot.id = "row_" + new_row_id +"_shot_" + i
+    select_shot.id = "row_" + new_row_id +"_shot_" + position
     select_shot.onchange = function () {update_score_total(new_row_id)}
     // X & 6 and V are exclusive depending on the shooter being TR or F-Class
     let html = '\
@@ -86,9 +86,10 @@ function add_shot(new_row_id, i){
     shots_input.appendChild(select_shot)
 }
 
-function  add_sighter_option(new_row_id, i){
+function  add_sighter_option(new_row_id, position){
     let sighter_option = document.createElement('select')
-    sighter_option.id = "row_" + new_row_id + "_sighter_" + i
+    sighter_option.id = "row_" + new_row_id + "_sighter_" + position
+    sighter_option.onchange = function () {update_to_counter(new_row_id, position)}
     let html = '\
         <option value="counter" id="counter">C</option>\
         <option  value="sighter" id= "sighter">S</option>'
@@ -115,18 +116,36 @@ function toDateInputValue(dateObject){
     return local.toJSON().slice(0,10);
 }
 
-//Update score total on changes to the rows score selects
-//TODO: Handle sighters, handle diffrent shoot lengths
+//Update score total on changes to the rows score selects or sighters
+let unconverted_sighters = 0
 function update_score_total(row_id){
     total_score = 0
-    for (let i = 0; i < 7; i++){
+    for (let i = 0 + unconverted_sighters; i < (match_type[0]["match_counters"] + unconverted_sighters); i++){
         let shot_value = document.getElementById('row_' + row_id + '_shot_' + i).value
         total_score = parseFloat(total_score) + parseFloat(shot_value)
     }
     document.getElementById('score_' + row_id).innerHTML = total_score.toFixed(2)
 }
 
-function update_sighters(row_id, sighter_id){
-    let sighter_value = document.getElementById('row_' + row_id + '_sighter_' + sighter_id).value
-    //if 
+//Handles changes to sighter select state
+function update_to_counter(row_id, position){
+    new_shot_pos = document.getElementById('shots_input_' + row_id).childElementCount - 2
+    add_shot(row_id, new_shot_pos)
+
+    unconverted_sighters++
+    update_score_total(row_id)
+
+    let sighter_value = document.getElementById('row_' + row_id + '_sighter_' + position)
+    sighter_value.onchange = function () {update_to_sighter(row_id, position)}
+}
+
+function update_to_sighter(row_id, position){
+    last_shot_pos = document.getElementById('shots_input_' + row_id).childElementCount - 3
+    document.getElementById('row_' + row_id + '_shot_' + last_shot_pos).remove()
+
+    unconverted_sighters--
+    update_score_total(row_id)
+    
+    let sighter_value = document.getElementById('row_' + row_id + '_sighter_' + position)
+    sighter_value.onchange = function () {update_to_counter(row_id, position)}
 }
