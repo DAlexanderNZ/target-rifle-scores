30f3a33103608c62b6dc1016e6cc4ebe3fde07b4
Daniel Alexander <5389473+DanAlexand@users.noreply.github.com>
2024-04-03 21:54:03 +1300

    continued with working on shots and class selection

--- static/js/script.js
+++ static/js/script.js
@@ -8,22 +8,26 @@ document.addEventListener('DOMContentLoaded', function(){
 function match_type_data(data){
     return JSON.parse(data)
 }
+
 //Define and render rows and contents for the table
-const score_input_table = document.getElementById('score_input')
-//increment row_id to allow for unique row ids and existing_rows to existing rows in the table
 let row_id = 0
-let existing_rows = []
+const score_input_table = document.getElementById('score_input')
 //TODO: Add Flask logic to load type_class's available from database.
-//TODO: Add logic to enable X & 6 / disable V for F-Class shooters
+class_types = '<select>\
+            <option onchange="update_to_tr(' + row_id + ')">TR</option>\
+            <option onchange="update_to_fclass(' + row_id + ')">FTR</option>\
+            <option onchange="update_to_fclass(' + row_id + ')">F-Open</option>\
+            </select>'
+
+//increment row_id to allow for unique row ids and existing_rows to existing rows in the table
 function add_row(){
     console.log("create_row")
     row_id++
-    existing_rows.push(row_id)
     let new_row = document.createElement('tr')
     new_row.id = "new_row_" + row_id
     let html = '<td id="button_' + row_id + '"><button type="button" onclick="add_row()" id="row_add_button_' + row_id + '">➕</button></td>\
         <td><input type="text" name="name" required></td>\
-        <td id="type_class"><select><option default>TR</option><option>FTR</option><option>F-Open</option></select></td>\
+        <td id="type_class">' + class_types + '</td>\
         <td id="shots_input_' + row_id + '"></td>\
         <td><span type="float" step="0.01" name="score" id="score_' + row_id + '" required></span></td>'
     new_row.innerHTML = html
@@ -47,17 +51,16 @@ function add_row(){
 
 //Allow for selected row to be removed. TODO: Check if user has input in the row and confirm choice.
 function remove_row(removal_row_id){
-    existing_rows.splice(removal_row_id -1)
-    console.log("Remove row " + removal_row_id + "\n Existing rows " + existing_rows)
+    console.log("Remove row " + removal_row_id)
     if (score_input_table.rows.length > 2) {
         console.log("Remove row " +  removal_row_id + ' new_row_' + removal_row_id)
         document.getElementById('new_row_' + removal_row_id).remove()
-        //TODO: Fix this logic to check all rows in table and ➕ to last row if none found
+        //TODO: Fix this logic to check all rows in table and ➕ to last row if none found or if extra
         if (removal_row_id == row_id){
             let last_row = document.getElementById('row_add_button_' + (row_id - 1))
             last_row.innerHTML = "➕"
             last_row.setAttribute('onclick', 'add_row()')
-        }
+        }     
     } else {
        alert('You cannot delete the last row of the table')
     }
@@ -73,9 +76,9 @@ function add_shot(new_row_id, position){
     select_shot.onchange = function () {update_score_total(new_row_id)}
     // X & 6 and V are exclusive depending on the shooter being TR or F-Class
     let html = '\
-        <option value="6.01" id="X"disabled>X</option>\
-        <option value="5.01" id="V">V</option\
-        <option value="6" id="6" disabled>6</option>\
+        <option value="6.01" id="X_' + new_row_id + position +'"disabled>X</option>\
+        <option value="5.01" id="V_' + new_row_id + position +'">V</option\
+        <option value="6" id="6_' + new_row_id + position +'" disabled>6</option>\
         <option value="5" selected>5</option>\
         <option value="4">4</option>\
         <option value="3">3</option>\
@@ -86,7 +89,9 @@ function add_shot(new_row_id, position){
     shots_input.appendChild(select_shot)
 }
 
-function  add_sighter_option(new_row_id, position){
+
+//Sighter as select
+/*function  add_sighter_option(new_row_id, position){
     let sighter_option = document.createElement('select')
     sighter_option.id = "row_" + new_row_id + "_sighter_" + position
     sighter_option.onchange = function () {update_to_counter(new_row_id, position)}
@@ -94,6 +99,15 @@ function  add_sighter_option(new_row_id, position){
         <option value="counter" id="counter">C</option>\
         <option  value="sighter" id= "sighter">S</option>'
     sighter_option.innerHTML = html
+    shots_input.appendChild(sighter_option)
+}*/
+
+//Sighter as checkbox
+function  add_sighter_option(new_row_id, position){
+    let sighter_option = document.createElement('input')
+    sighter_option.type = "checkbox"
+    sighter_option.id = "row_" + new_row_id + "_sighter_" + position
+    sighter_option.onchange = function () {update_to_counter(new_row_id, position)}
     shots_input.appendChild(sighter_option)
 }
 
@@ -128,24 +142,52 @@ function update_score_total(row_id){
 }
 
 //Handles changes to sighter select state
+//TODO: Enforce rule that first sigher can only be converted with the second sighter
 function update_to_counter(row_id, position){
     new_shot_pos = document.getElementById('shots_input_' + row_id).childElementCount - 2
     add_shot(row_id, new_shot_pos)
-
     unconverted_sighters++
-    update_score_total(row_id)
-
+    //Incorrect first at enforcing sighter rule
+    /*if (position == match_type[0]['match_sighters'] - 1){
+        new_shot_pos = document.getElementById('shots_input_' + row_id).childElementCount - 2
+        add_shot(row_id, new_shot_pos)
+        let sighter_value = document.getElementById('row_' + row_id + '_sighter_' + position - 1)
+        sighter_value.checked
+        sighter_value.onchange = function () {update_to_sighter(row_id, position)}
+    }*/
     let sighter_value = document.getElementById('row_' + row_id + '_sighter_' + position)
     sighter_value.onchange = function () {update_to_sighter(row_id, position)}
+    
+    update_score_total(row_id)
 }
 
 function update_to_sighter(row_id, position){
     last_shot_pos = document.getElementById('shots_input_' + row_id).childElementCount - 3
     document.getElementById('row_' + row_id + '_shot_' + last_shot_pos).remove()
 
-    unconverted_sighters--
-    update_score_total(row_id)
-    
     let sighter_value = document.getElementById('row_' + row_id + '_sighter_' + position)
     sighter_value.onchange = function () {update_to_counter(row_id, position)}
+    
+    unconverted_sighters--
+    update_score_total(row_id)
 }
+
+//Shooter class type change handling. Not yet working
+function update_to_fclass(row_id){
+    console.log("Update to F-Class")
+    for (i = 0; i < document.getElementById('shots_input_' + row_id).childElementCount - 2; i++){
+        document.getElementById('X_' + row_id + i).removeAttribute('disabled', 'disabled')
+        document.getElementById('6_' + row_id + i).removeAttribute('disabled', 'disabled')
+        document.getElementById('V_' + row_id + i).setAttribute('disabled')
+    }
+}
+
+function update_to_tr(row_id){
+    console.log("Update to TR")
+    for (i = 0; i < document.getElementById('shots_input_' + row_id).childElementCount - 2; i++){
+        document.getElementById('X_' + row_id + i).setAttribute('disabled')
+        document.getElementById('6_' + row_id + i).setAttribute('disabled')
+        document.getElementById('V_' + row_id + i).removeAttribute('disabled', 'disabled')
+    }
+}
+
